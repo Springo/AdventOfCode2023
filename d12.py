eefca6c4cdf4e99812cb48b7df440218d11931df
Kevin <kevinmxia@gmail.com>
2023-12-12 00:47:46 -0500

    clean up day 12

--- d12.py
+++ d12.py
@@ -10,47 +10,6 @@ def readFile(filename):
     return lines
 
 
-def check_match(line, spacings):
-    ls = line.split('.')
-    ind = 0
-    for g in ls:
-        if len(g) == 0:
-            continue
-        if ind >= len(spacings):
-            return False
-        if len(g) == spacings[ind]:
-            ind += 1
-        else:
-            return False
-    if ind < len(spacings):
-        return False
-    return True
-
-
-def count_matches_slow(line, spacings):
-    count = 0
-    line_len = line.count('?')
-    for combo in range(2 ** line_len):
-        bs = bin(combo)[2:]
-        bs = "".join(['0'] * (line_len - len(bs))) + bs
-        new_line = ""
-        ind = 0
-        for c in line:
-            if c == '?':
-                if bs[ind] == '0':
-                    new_line += "."
-                elif bs[ind] == '1':
-                    new_line += "#"
-                else:
-                    print("AHHH")
-                ind += 1
-            else:
-                new_line += c
-        if check_match(new_line, spacings):
-            count += 1
-    return count
-
-
 def count_matches(line, spacings, memo):
     def check_fit(line, count):
         if count > len(line):
@@ -89,6 +48,7 @@ def count_matches(line, spacings, memo):
     memo[key] = total
     return total
 
+
 lines = readFile("d12input.txt")
 total = 0
 for line in lines:
@@ -98,6 +58,5 @@ for line in lines:
     l_inp = "".join(list(ls[0] + "?") * 5)[:-1]
     spacings = spacings * 5
     count = count_matches(l_inp, spacings, memo)
-    print("{}, {}: {}".format(l_inp, spacings, count))
     total += count
 print(total)
